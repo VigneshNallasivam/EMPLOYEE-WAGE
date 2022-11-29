0ee0b0a732ceb3e70199f42e8b241e14a58a6342
VigneshNallasivam <vickyshanestark@gmail.com>
2022-11-29 13:21:55 +0530

    Added : UC-5 Using Switch Case for Calculating Monthly Wage of an Employee

---
+++ src/com/Java/Basics/MonthlyWage.java
@@ -0,0 +1,49 @@
+package com.Java.Basics;
+import java.util.Random;
+public class MonthlyWage 
+{
+	public static void main(String[] args)
+	{
+	Random random = new Random();
+	int empCheck = random.nextInt(3);
+	int dailywage;
+	int halfDayWage;
+	int wagePerHour = 20;
+	int fullDayHour = 8;
+	int partDayHour = 4;
+	int monthlyWage1, monthlyWage2;
+	int totalWorkingDays = 20;
+	int totalWorkingHours = 0; 
+	
+	/*
+	 * Calculating Daily,Part-time,Absent & Month Wage of Employee
+	 */
+	
+	switch(empCheck)
+	{
+	case 0: System.out.println("Employee is Absent.");
+	        System.out.println("Absent Wage Is : - Nil");
+	        System.out.println("Monthly Wage Is : - Nil");
+	        System.out.println("Total Working Hours Is : - Nil");
+	        break;
+	        
+	case 1: System.out.println("Employee is Present.");
+	        dailywage = wagePerHour * fullDayHour;
+	        System.out.println("Daily Wage Is : " +dailywage);
+	        monthlyWage1 = totalWorkingDays * dailywage;
+	        System.out.println("Monthly Wage Is : " +monthlyWage1);
+	        totalWorkingHours = 20 * partDayHour * fullDayHour;
+	        System.out.println("Total Working Hours Is : " +totalWorkingHours);
+	        break;
+	        
+   default: System.out.println("Employee is Half-Day Present");
+	        halfDayWage = wagePerHour * partDayHour;
+	        System.out.println("Half Day Wage Is : " +halfDayWage);
+	        monthlyWage2 = totalWorkingDays * halfDayWage;
+	        System.out.println("Monthly Wage Is : " +monthlyWage2);
+	        totalWorkingHours = 20 * partDayHour * fullDayHour;
+	        System.out.println("Total Working Hours Is : " +totalWorkingHours);
+	        break;
+	}
+  }
+}
